932e3b7715a8bc036b5bbc694c715fb732014a43
Arie Dwi Kusuma <69567276+Arie-Kusuma@users.noreply.github.com>
2020-09-16 18:32:20 +0700

    nambahin clock

--- m/index.php
+++ m/index.php
@@ -19,11 +19,41 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-</head>
-<body>
+	<title>Sistem Ujian Essai</title>
+	<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+	<script type="text/javascript" src="../js/jquery.waves.js"></script>
+	<link rel="stylesheet" type="text/css" href="./../style/jquery.waves.min.css">
+	<link rel="sortcut icon" type="image/x-icon" href="../faviconn.ico"></head>
+<body id="m-index">
 
-	coba
+	<div id="m-top-title">
+					<h2 id="m-title">
+						Sistem Ujian Essai <mark id="m-dc">-open beta-</mark>
+					</h2>
+	</div>
 
+	<div id="mid-title">
+		<h1 id="date">{time.now}</h1>
+	</div>
+
+
+<script type="text/javascript">
+	 function clockTick()    {
+       currentTime = new Date();
+       month = currentTime.getMonth() + 1;
+       day = currentTime.getDate();
+       year = currentTime.getFullYear();
+       jam = ('0'+currentTime.getHours()).slice(-2);
+       menit = ('0'+currentTime.getMinutes()).slice(-2);
+      // alert("hi");
+      document.getElementById('date').innerHTML=day + "/" + month + "/" + year + " | "+ jam + ":" + menit ;
+    }
+    
+
+
+   setInterval(function(){clockTick();}, 1000);
+
+</script>
 </body>
 </html>
